1ef1f3f8ed10025389dbbd41d32c0b1a80946172
Klyse <Klaus.Pruenster@gmail.com>
2020-11-10 17:51:39 +0100

    added mongodb telemetrics

--- Persistence/StockDb.cs
+++ Persistence/StockDb.cs
@@ -1,6 +1,9 @@
-﻿using Application.Services;
+﻿using System;
+using Application.Services;
 using Domain;
+using Microsoft.ApplicationInsights;
 using MongoDB.Driver;
+using MongoDB.Driver.Core.Events;
 
 namespace Persistence
 {
@@ -8,13 +11,29 @@ namespace Persistence
 	{
 		private readonly MongoClient _client;
 		private readonly IMongoDatabase _db;
+		private readonly TelemetryClient _telemetryClient;
 
-		public StockDb(IConfigService config)
+		public StockDb(IConfigService config, TelemetryClient telemetryClient)
 		{
+			_telemetryClient = telemetryClient;
 			var connectionString = config.GetMongoDbConnectionString();
-			_client = new MongoClient(connectionString);
-			var databaseName = MongoUrl.Create(connectionString).DatabaseName;
-			_db = _client.GetDatabase(databaseName);
+			var mongoUrl = MongoUrl.Create(connectionString);
+			var mongoClientSettings = MongoClientSettings.FromUrl(mongoUrl);
+
+			mongoClientSettings.ClusterConfigurator = clusterConfigurator =>
+			{
+				clusterConfigurator.Subscribe<CommandSucceededEvent>(e =>
+				{
+					if (e.OperationId == null)
+						return;
+
+					_telemetryClient.TrackDependency("database", "mongodb", e.CommandName, DateTimeOffset.UtcNow.Subtract(e.Duration), e.Duration, true);
+				});
+				clusterConfigurator.Subscribe<CommandFailedEvent>(e => { _telemetryClient.TrackDependency("database", "mongodb", e.CommandName, DateTimeOffset.UtcNow.Subtract(e.Duration), e.Duration, false); });
+			};
+
+			_client = new MongoClient(mongoClientSettings);
+			_db = _client.GetDatabase(mongoUrl.DatabaseName);
 
 			Chats = _db.GetCollection<Chat>(nameof(Chat));
 			Symbols = _db.GetCollection<Symbol>(nameof(Symbol));
