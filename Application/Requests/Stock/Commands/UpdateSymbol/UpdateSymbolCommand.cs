c9f2b8a234f37f881393b9bea382b7f6f12669c9
Klyse <Klaus.Pruenster@gmail.com>
2020-11-12 15:46:37 +0100

    added update symbol command

---
+++ Application/Requests/Stock/Commands/UpdateSymbol/UpdateSymbolCommand.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Services;
+using Application.Services.StockService;
+using Domain;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+
+namespace Application.Requests.Stock.Commands.UpdateSymbol
+{
+	public class UpdateSymbolCommand : IRequest
+	{
+		public UpdateSymbolCommand(string name)
+		{
+			Name = name;
+		}
+
+		public string Name { get; }
+
+		public class Handler : IRequestHandler<UpdateSymbolCommand, Unit>
+		{
+			private const int KeepHistoryDays = 10;
+			private readonly ILogger<UpdateSymbolCommand> _log;
+			private readonly IStockDb _stockDb;
+			private readonly IStockService _stockService;
+
+			public Handler(IStockDb stockDb, IStockService stockService, ILogger<UpdateSymbolCommand> log)
+			{
+				_stockDb = stockDb;
+				_stockService = stockService;
+				_log = log;
+			}
+
+			public async Task<Unit> Handle(UpdateSymbolCommand request, CancellationToken cancellationToken)
+			{
+				Quote symbolQuote;
+				try
+				{
+					symbolQuote = await _stockService.GetQuoteAsync(request.Name);
+				}
+				catch (Exception e)
+				{
+					_log.LogError(e, "Cannot get quote for symbol {Name}", request.Name);
+					return Unit.Value;
+				}
+
+				var now = DateTimeOffset.UtcNow;
+
+				// update fields
+				_stockDb.Symbols.FindOneAndUpdate<Symbol>(r => r.Name == request.Name,
+					new UpdateDefinitionBuilder<Symbol>()
+						.AddToSet(r => r.PriceHistory, new SymbolPriceHistory
+						{
+							TimeStamp = now.UtcDateTime,
+							Price = symbolQuote.Price
+						})
+						.Set(r => r.LastPrice, symbolQuote.Price)
+						.Set(r => r.LastChangePercent, symbolQuote.ChangePercent)
+						.Set(r => r.LastInfo, now.UtcDateTime)
+						.SetOnInsert(r => r.Name, request.Name),
+					new FindOneAndUpdateOptions<Symbol, Symbol>
+					{
+						IsUpsert = true
+					},
+					cancellationToken
+				);
+
+				// remove old Price History
+				_stockDb.Symbols.FindOneAndUpdate(r => r.Name == request.Name,
+					new UpdateDefinitionBuilder<Symbol>()
+						.PullFilter(r => r.PriceHistory, r => r.TimeStamp < now.UtcDateTime.AddDays(-KeepHistoryDays)),
+					cancellationToken: cancellationToken
+				);
+
+				return Unit.Value;
+			}
+		}
+	}
+}
--- Application/Requests/Stock/Commands/UpdateSymbols/UpdateSymbolsCommand.cs
+++ Application/Requests/Stock/Commands/UpdateSymbols/UpdateSymbolsCommand.cs
@@ -2,11 +2,9 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Requests.Stock.Commands.UpdateSymbol;
 using Application.Services;
-using Application.Services.StockService;
-using Domain;
 using MediatR;
-using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 
 namespace Application.Requests.Stock.Commands.UpdateSymbols
@@ -16,18 +14,15 @@ namespace Application.Requests.Stock.Commands.UpdateSymbols
 		public class Handler : IRequestHandler<UpdateSymbolsCommand, Unit>
 		{
 			private const int UpdateIntervalMin = 10;
-			private const int KeepHistoryDays = 10;
-			private readonly ILogger<Handler> _log;
+			private readonly IMediator _mediator;
 			private readonly IStockDb _stockDb;
-			private readonly IStockService _stockService;
 			private readonly ITelemetryService _telemetryService;
 
-			public Handler(IStockDb stockDb, IStockService stockService, ILogger<Handler> log, ITelemetryService telemetryService)
+			public Handler(IStockDb stockDb, ITelemetryService telemetryService, IMediator mediator)
 			{
 				_stockDb = stockDb;
-				_stockService = stockService;
-				_log = log;
 				_telemetryService = telemetryService;
+				_mediator = mediator;
 			}
 
 			public async Task<Unit> Handle(UpdateSymbolsCommand request, CancellationToken cancellationToken)
@@ -50,47 +45,7 @@ namespace Application.Requests.Stock.Commands.UpdateSymbols
 
 				_telemetryService.TrackSymbolsToRefresh(symbols.Count);
 
-				foreach (var symbol in symbols)
-				{
-					Quote symbolQuote;
-					try
-					{
-						symbolQuote = await _stockService.GetQuoteAsync(symbol);
-					}
-					catch (Exception e)
-					{
-						_log.LogError(e, "Cannot get quote for symbol {Name}", symbol);
-						continue;
-					}
-
-					var now = DateTimeOffset.UtcNow;
-
-					// update fields
-					_stockDb.Symbols.FindOneAndUpdate<Symbol>(r => r.Name == symbol,
-						new UpdateDefinitionBuilder<Symbol>()
-							.AddToSet(r => r.PriceHistory, new SymbolPriceHistory
-							{
-								TimeStamp = now.UtcDateTime,
-								Price = symbolQuote.Price
-							})
-							.Set(r => r.LastPrice, symbolQuote.Price)
-							.Set(r => r.LastChangePercent, symbolQuote.ChangePercent)
-							.Set(r => r.LastInfo, now.UtcDateTime)
-							.SetOnInsert(r => r.Name, symbol),
-						new FindOneAndUpdateOptions<Symbol, Symbol>
-						{
-							IsUpsert = true
-						},
-						cancellationToken
-					);
-
-					// remove old Price History
-					_stockDb.Symbols.FindOneAndUpdate(r => r.Name == symbol,
-						new UpdateDefinitionBuilder<Symbol>()
-							.PullFilter(r => r.PriceHistory, r => r.TimeStamp < now.UtcDateTime.AddDays(-KeepHistoryDays)),
-						cancellationToken: cancellationToken
-					);
-				}
+				foreach (var symbol in symbols) await _mediator.Send(new UpdateSymbolCommand(symbol), cancellationToken);
 
 				return Unit.Value;
 			}
